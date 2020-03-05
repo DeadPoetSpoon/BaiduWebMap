3f5e234fc6e8c79150a7bcca11f41019bc2355e0
DeadPoetSpoon <1149097040@qq.com>
2020-03-05 22:20:05 +0800

    几乎完成基础面板所有功能，包括调用浏览器定位、点击地图拾取坐标、多位置定位显示（Marker&经纬度与地区名称互相解析）、智能提示定位、多关键词检索、矩形区域内的检索、圆形区域内的检索

--- js/bdmap.js
+++ js/bdmap.js
@@ -1,6 +1,9 @@
 import {showAlert,popAlert} from './showAlert.js';
-//显示欢迎信息
+//显示欢迎信息,并在10秒后删除
 showAlert("info","欢迎！",'这是基于<a href="http://lbsyun.baidu.com/" class="alert-link"><strong>百度API</strong></a>简单实现的WebGIS网页。');
+setTimeout(function () {
+    popAlert(1);
+}, 3000 )
 //构造全局变量map
 var map = new BMap.Map("BdMapContainer");
 //map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  
@@ -106,6 +109,7 @@ function isLngLat(str) {
 }
 function getLngLatLocation(myGeoCoder,lng,lat) {
     myGeoCoder.getLocation(new BMap.Point(lng,lat), function(rs){
+        map.centerAndZoom(new BMap.Point(lng,lat), 16);
         var addComp = rs.addressComponents;
         //alert(addComp.province + ", " + addComp.city + ", " + addComp.district + ", " + addComp.street + ", " + addComp.streetNumber);
         addMarker(new BMap.Point(lng,lat),addComp.province + addComp.city  + addComp.district  + addComp.street  + addComp.streetNumber);
@@ -138,13 +142,47 @@ function addMarker(point,text){
         map.removeOverlay(marker);
     });
 }
-
+//初始化地图
 initMap();
 //基础面板初始化
+//初始化自动补全
+var ac = new BMap.Autocomplete(    //建立一个自动完成的对象
+    {"input" : "ACSearchCon"
+    ,"location" : map
+});
+ac.addEventListener("onconfirm", function(e) {    //鼠标点击下拉列表后的事件
+	var _value = e.item.value;
+	var myValue = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+    $("#ACSearchCon").val(myValue);
+    getTextLocation(new BMap.Geocoder(),myValue);
+});
+$("#Btn_ACSearch").click(function (e) { 
+    e.preventDefault();
+    var text = $("#ACSearchCon").val();
+    if(text.length<1){
+        showAlert("danger","输入为空！","<strong>请在输入框输入搜索内容。</strong>");
+    }
+    getTextLocation(new BMap.Geocoder(),text);
+});
 //添加定位按钮点击事件
 $("#Btn_MyLocal").click(function (e) { 
     e.preventDefault();
     getCurLocation();
+});
+//添加坐标拾取点击事件
+var nClickLngLat = 0;
+var lnglatevent = function (e) {
+    $("#ClickLngLatCon").val(e.point.lng + ", " + e.point.lat);
+}
+$("#Btn_ClickLngLat").click(function () { 
+    if(nClickLngLat%2==0){
+        //console.log("选中");
+        map.addEventListener("click",lnglatevent);
+    }else{
+        //console.log("未选中");
+        map.removeEventListener("click",lnglatevent);
+    }
+    nClickLngLat++;
 });
 //添加输入搜索按钮点击事件
 $("#Btn_SearchLocal").click(function (e) { 
@@ -152,7 +190,7 @@ $("#Btn_SearchLocal").click(function (e) {
     var text = $("#SearchLocalCon").val();
     if(text.length<1){
         showAlert("danger","输入为空！","<strong>请在输入框输入搜索内容。</strong>");
-        popAlert(1);
+        //popAlert(1);
     }
     //console.log(text);
     var textArray = text.split(';');
@@ -170,4 +208,133 @@ $("#Btn_SearchLocal").click(function (e) {
 $("#clearOverlay0").click(function (e) { 
     e.preventDefault();
     map.clearOverlays();
+});
+//添加关键字检索按钮点击事件
+$("#Btn_KeySearch").click(function (e) { 
+    e.preventDefault();
+    if($("#KeySearchCon").val().length==0){
+        showAlert("danger","输入为空！","<strong>请在输入框输入搜索内容。</strong>");
+        return;
+    }
+    $("#ShHiRRuseult").show();
+    var myKeys = $("#KeySearchCon").val().split(';');
+    var local = new BMap.LocalSearch(map, {
+		renderOptions:{map: map, panel:"r-result"},
+    });
+    //console.log(myKeys.length);
+    if(myKeys.length==1){
+        local.setPageCapacity(4);
+    }else if(myKeys.length==2){
+        local.setPageCapacity(2);
+    }else{
+        local.setPageCapacity(1);
+    }
+    //local.searchInBounds(myKeys, map.getBounds());
+    local.search(myKeys);
+});
+//添加矩形区域检索按钮点击事件，写了两份有点多余
+$("#Btn_RectSearch").click(function (e) { 
+    e.preventDefault();
+    if($("#RectSearchCon").val().length==0){
+        showAlert("danger","输入为空！","<strong>请在输入框输入搜索内容。</strong>");
+        return;
+    }
+    $("#ShHiRectRuseult").show();
+    //配置颜色
+    var styleOptions = {
+        strokeColor:"red",    //边线颜色。
+        fillColor:"blue",      //填充颜色。当参数为空时，圆形将没有填充效果。
+        strokeWeight: 3,       //边线的宽度，以像素为单位。
+        strokeOpacity: 0.8,	   //边线透明度，取值范围0 - 1。
+        fillOpacity: 0.3,      //填充的透明度，取值范围0 - 1。
+        strokeStyle: 'solid' //边线的样式，solid或dashed。
+    }
+    //实例化鼠标绘制工具
+    var drawingManager = new BMapLib.DrawingManager(map, {
+        isOpen: true, //是否开启绘制模式
+        enableDrawingTool: false, //是否显示工具栏
+        drawingToolOptions: {
+            anchor: BMAP_ANCHOR_TOP_RIGHT, //位置
+            offset: new BMap.Size(5, 5), //偏离值
+        },
+        circleOptions: styleOptions, //圆的样式
+        polylineOptions: styleOptions, //线的样式
+        polygonOptions: styleOptions, //多边形的样式
+        rectangleOptions: styleOptions //矩形的样式
+    });
+    //设置模式会绘制矩形
+    drawingManager.setDrawingMode(BMAP_DRAWING_RECTANGLE);
+    showAlert("info","请绘制矩形","鼠标拖动来绘制。");
+	//添加鼠标绘制工具监听事件，用于获取绘制结果
+    drawingManager.addEventListener('rectanglecomplete',function (overlay) {
+        popAlert(1);
+        var myKeys = $("#RectSearchCon").val().split(';');
+        var local = new BMap.LocalSearch(map, {
+            renderOptions:{map: map, panel:"Rect-result"},
+        });
+        if(myKeys.length==1){
+            local.setPageCapacity(4);
+        }else if(myKeys.length==2){
+            local.setPageCapacity(2);
+        }else{
+            local.setPageCapacity(1);
+        }
+        //console.log('overlay :', overlay);
+        local.searchInBounds(myKeys, overlay.getBounds());
+        drawingManager.close();
+        map.clearOverlays();
+    });    
+});
+//添加圆形区域检索按钮点击事件，写了两份有点多余
+$("#Btn_CircleSearch").click(function (e) { 
+    e.preventDefault();
+    if($("#CircleSearchCon").val().length==0){
+        showAlert("danger","输入为空！","<strong>请在输入框输入搜索内容。</strong>");
+        return;
+    }
+    $("#ShHiCircleRuseult").show();
+    //配置颜色
+    var styleOptions = {
+        strokeColor:"red",    //边线颜色。
+        fillColor:"blue",      //填充颜色。当参数为空时，圆形将没有填充效果。
+        strokeWeight: 3,       //边线的宽度，以像素为单位。
+        strokeOpacity: 0.8,	   //边线透明度，取值范围0 - 1。
+        fillOpacity: 0.3,      //填充的透明度，取值范围0 - 1。
+        strokeStyle: 'solid' //边线的样式，solid或dashed。
+    }
+    //实例化鼠标绘制工具
+    var drawingManager = new BMapLib.DrawingManager(map, {
+        isOpen: true, //是否开启绘制模式
+        enableDrawingTool: false, //是否显示工具栏
+        drawingToolOptions: {
+            anchor: BMAP_ANCHOR_TOP_RIGHT, //位置
+            offset: new BMap.Size(5, 5), //偏离值
+        },
+        circleOptions: styleOptions, //圆的样式
+        polylineOptions: styleOptions, //线的样式
+        polygonOptions: styleOptions, //多边形的样式
+        rectangleOptions: styleOptions //矩形的样式
+    });
+    //设置模式会绘制矩形
+    drawingManager.setDrawingMode(BMAP_DRAWING_CIRCLE);
+    showAlert("info","请绘制圆形","鼠标拖动来绘制。");
+	//添加鼠标绘制工具监听事件，用于获取绘制结果
+    drawingManager.addEventListener('circlecomplete',function (overlay) {
+        popAlert(1);
+        var myKeys = $("#CircleSearchCon").val().split(';');
+        var local = new BMap.LocalSearch(map, {
+            renderOptions:{map: map, panel:"Circle-result"},
+        });
+        if(myKeys.length==1){
+            local.setPageCapacity(4);
+        }else if(myKeys.length==2){
+            local.setPageCapacity(2);
+        }else{
+            local.setPageCapacity(1);
+        }
+        //console.log('overlay :', overlay);
+        local.searchNearby(myKeys,overlay.getCenter(),overlay.getRadius());
+        drawingManager.close();
+        //map.clearOverlays();
+    });    
 });
